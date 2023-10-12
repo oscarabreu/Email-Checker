27f96d97957d324d22e007eddae36b2a7f9954b8
Oscar Abreu <oja227@nyu.edu>
2023-10-12 12:14:25 -0400

    Clarity

--- main.go
+++ main.go
@@ -9,17 +9,22 @@ import (
 	"strings"
 )
 
+const (
+	SPFPrefix   = "v=spf1"
+	DMARCPrefix = "v=DMARC1"
+	DMARCRecordPrefix = "_dmarc."
+)
+
 func main() {
 	// We use a Scanner to buffer the input stream and read line by line
 	// This prevents loading the entire input into memory at once!
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord\n")
-
-	// Read each line from the input stream and call our controller function
+	// Read eachline from the input stream and call our controller function
 	for scanner.Scan() {
-		inspectDomain(scanner.Text())
+		domain := scanner.Text()
+		inspectDomain(domain)
 	}
-
 	// Error handling
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("Error: could not read from input: %v\n", err)
@@ -27,17 +32,17 @@ func main() {
 }
 
 // inspectDomain takes a domain name and prints a CSV line with the results
-func inspectDomain(targetDomain string) {
-	isMXPresent := validateMX(targetDomain) // Checks if MX records exist
-	isSPFPresent, detectedSPF := detectSPF(targetDomain) // Checks if SPF records exist and returns the record
-	isDMARCPresent, detectedDMARC := detectDMARC(targetDomain) // Checks if DMARC records exist and returns the record
+func inspectDomain(domain string) {
+	isMXPresent := detectMX(domain) // Checks if MX records exist
+	isSPFPresent, detectedSPF := detectSPF(domain) // Checks if SPF records exist and returns the record
+	isDMARCPresent, detectedDMARC := detectDMARC(domain) // Checks if DMARC records exist and returns the record
 
-	fmt.Printf("%v, %v, %v, %q, %v, %q\n", targetDomain, isMXPresent, isSPFPresent, detectedSPF, isDMARCPresent, detectedDMARC)
+	fmt.Printf("%v, %v, %v, %q, %v, %q\n", domain, isMXPresent, isSPFPresent, detectedSPF, isDMARCPresent, detectedDMARC)
 }
 
 // Function to validate MX records using LookupMX from the net package.
 // Returns true if MX records exist (more than 0), false if not.
-func validateMX(domainName string) bool {
+func detectMX(domainName string) bool {
 	mxEntries, mxErr := net.LookupMX(domainName)
 	// Error handling.
 	if mxErr != nil {
@@ -58,7 +63,7 @@ func detectSPF(domainName string) (bool, string) {
 	}
 	// Loop through the TXT entries and check if any of them start with "v=spf1"
 	for _, entry := range txtEntries {
-		if strings.HasPrefix(entry, "v=spf1") {
+		if strings.HasPrefix(entry, SPFPrefix) {
 			return true, entry
 		}
 	}
@@ -70,7 +75,7 @@ func detectSPF(domainName string) (bool, string) {
 func detectDMARC(domainName string) (bool, string) {
 	// To check the DMARC record for example.com, 
 	// you'd look up the TXT records for _dmarc.example.com.
-	dmarcEntries, dmarcErr := net.LookupTXT("_dmarc." + domainName)
+	dmarcEntries, dmarcErr := net.LookupTXT(DMARCRecordPrefix + domainName)
 	// Error handling.
 	if dmarcErr != nil {
 		log.Printf("DMARC Lookup Error: %v\n", dmarcErr)
@@ -78,7 +83,7 @@ func detectDMARC(domainName string) (bool, string) {
 	}
 	// Loop through the TXT entries and check if any of them start with "v=DMARC1"
 	for _, entry := range dmarcEntries {
-		if strings.HasPrefix(entry, "v=DMARC1") {
+		if strings.HasPrefix(entry, DMARCPrefix) {
 			return true, entry
 		}
 	}
